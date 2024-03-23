339884722372abe63b679deb6e878fa2d0bb3e5e
Maglojata <112628977+Maglojata@users.noreply.github.com>
2024-03-23 12:27:25 +0400

    Update main.cpp

--- Task1_new_delete_operators/src/main.cpp
+++ Task1_new_delete_operators/src/main.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
 #include "../header/new_delete.h"
 
-
 int main() {
-   
-   	int* p;
-	p = my_new(5);
-    	std::cout << *p << std::endl;
-    	*p = 55;
-    	std::cout << *p << std::endl;
 
-    	int *pp = my_new_arr<int>(5);
+	int *pp = my_new_arr<int>(5);
     	for (size_t i = 0; i < 5; ++i){
         	std::cin >>pp[i];
     	}
